3f3538f373050eb28d95915bdd6d2c0a2b12143f
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-05-23 13:05:53 +0300

    g

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -4,13 +4,13 @@
       <h1 class="font-bold text-[4.5rem]">
         Design, Develop, Deliver.<br> Software Engineer.
       </h1>
-      <div class="flex space-x-6 mt-10 flex-wrap justify-start space-y-6">
+      <!-- <div class="flex space-x-6 mt-10 flex-wrap justify-start space-y-6">
         <img src="../assets/images/laravel.png" class="rounded-xl" alt="" srcset="" width="70">
         <img src="../assets/images/vue.png" alt="" srcset="" width="70">
         <img src="../assets/images/react.png" alt="" srcset="" width="70">
         <img src="../assets/images/Inertiajs.png" class="rounded-xl" alt="" srcset="" width="60">
         <img src="../assets/images/tailwindcss.png" alt="" srcset="" width="60">
-      </div>
+      </div> -->
     </div>
 
 
@@ -28,7 +28,7 @@
           <p class="text-sm  text-slate-500">React Native, Redux</p>
         </div>
       </div>
-      <!-- <div class="max-w-[700px] flex items-center flex-col mt-40">
+      <div class="max-w-[700px] flex items-center flex-col mt-40">
         <a href="#" class="cursor-pointer scale-100 transition-all duration-700 hover:scale-[90%]">
           <img src="../assets/images/p1.png" class="rounded-2xl" />
         </a>
@@ -37,7 +37,30 @@
           <h3 class="text-xl text-slate-700">Company website</h3>
           <p class="text-sm  text-slate-500">Vuejs Laravel</p>
         </div>
-      -->
+      </div>
+      <div class="max-w-[700px] flex items-center flex-col">
+        <a href="#" class="cursor-pointer scale-100 transition-all duration-700 hover:scale-[90%]">
+          <img src="../assets/images/p1.png" class="rounded-2xl" width="540" />
+        </a>
+        <div class="mt-4 flex items-center flex-col">
+          <a target="_blank" href="https://play.google.com/store/apps/details?id=com.mohamadelhadidy.bmicalculator"
+            class="hover:underline">
+            <h1 class="font-bold text-2xl">BMI Calculator: Ideal Weight</h1>
+          </a>
+          <h3 class="text-xl text-slate-700">Freelance Android App</h3>
+          <p class="text-sm  text-slate-500">React Native, Redux</p>
+        </div>
+      </div>
+      <div class="max-w-[700px] flex items-center flex-col mt-40">
+        <a href="#" class="cursor-pointer scale-100 transition-all duration-700 hover:scale-[90%]">
+          <img src="../assets/images/p1.png" class="rounded-2xl" />
+        </a>
+        <div class="mt-4 flex items-center flex-col">
+          <h1 class="font-bold text-2xl">Days</h1>
+          <h3 class="text-xl text-slate-700">Company website</h3>
+          <p class="text-sm  text-slate-500">Vuejs Laravel</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
