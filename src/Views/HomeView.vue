3be269e86f246f712adf5d17f0f7753bc4fb782e
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-05-23 12:44:51 +0300

    g

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -1,20 +1,36 @@
 <template>
   <div class=" max-w-[1200px] mx-auto">
-    <h1 class="pt-60 pb-40 font-bold text-[4.5rem]">
-      Design, Develop, Deliver.<br> Full-stack Web Developer.
-    </h1>
+    <div class="flex flex-col pt-60 pb-40 ">
+      <h1 class="font-bold text-[4.5rem]">
+        Design, Develop, Deliver.<br> Software Engineer.
+      </h1>
+      <div class="flex space-x-6 mt-10 flex-wrap justify-start space-y-6">
+        <img src="../assets/images/php.png" alt="" srcset="" width="70">
+        <img src="../assets/images/laravel.png" class="rounded-xl" alt="" srcset="" width="60">
+        <img src="../assets/images/mysql.png" alt="" srcset="" width="60">
+        <img src="../assets/images/vue.png" alt="" srcset="" width="60">
+        <img src="../assets/images/react.png" alt="" srcset="" width="60">
+        <img src="../assets/images/Inertiajs.png" class="rounded-xl" alt="" srcset="" width="60">
+        <img src="../assets/images/tailwindcss.png" alt="" srcset="" width="60">
+      </div>
+    </div>
+
+
     <div class="pb-40  max-w-[1200px] flex justify-between flex-wrap">
       <div class="max-w-[700px] flex items-center flex-col">
         <a href="#" class="cursor-pointer scale-100 transition-all duration-700 hover:scale-[90%]">
-          <img src="../assets/images/p1.png" class="rounded-2xl" />
+          <img src="../assets/images/p1.png" class="rounded-2xl" width="540" />
         </a>
         <div class="mt-4 flex items-center flex-col">
-          <h1 class="font-bold text-2xl">Days</h1>
-          <h3 class="text-xl text-slate-700">Company website</h3>
-          <p class="text-sm  text-slate-500">Vuejs Laravel</p>
+          <a target="_blank" href="https://play.google.com/store/apps/details?id=com.mohamadelhadidy.bmicalculator"
+            class="hover:underline">
+            <h1 class="font-bold text-2xl">BMI Calculator: Ideal Weight</h1>
+          </a>
+          <h3 class="text-xl text-slate-700">Freelance Android App</h3>
+          <p class="text-sm  text-slate-500">React Native, Redux</p>
         </div>
       </div>
-      <div class="max-w-[700px] flex items-center flex-col mt-40">
+      <!-- <div class="max-w-[700px] flex items-center flex-col mt-40">
         <a href="#" class="cursor-pointer scale-100 transition-all duration-700 hover:scale-[90%]">
           <img src="../assets/images/p1.png" class="rounded-2xl" />
         </a>
@@ -23,27 +39,7 @@
           <h3 class="text-xl text-slate-700">Company website</h3>
           <p class="text-sm  text-slate-500">Vuejs Laravel</p>
         </div>
-      </div>
-      <div class="max-w-[700px] flex items-center flex-col">
-        <a href="#" class="cursor-pointer scale-100 transition-all duration-700 hover:scale-[90%]">
-          <img src="../assets/images/p1.png" class="rounded-2xl" />
-        </a>
-        <div class="mt-4 flex items-center flex-col">
-          <h1 class="font-bold text-2xl">Days</h1>
-          <h3 class="text-xl text-slate-700">Company website</h3>
-          <p class="text-sm  text-slate-500">Vuejs Laravel</p>
-        </div>
-      </div>
-      <div class="max-w-[700px] flex items-center flex-col mt-40">
-        <a href="#" class="cursor-pointer scale-100 transition-all duration-700 hover:scale-[90%]">
-          <img src="../assets/images/p1.png" class="rounded-2xl" />
-        </a>
-        <div class="mt-4 flex items-center flex-col">
-          <h1 class="font-bold text-2xl">Days</h1>
-          <h3 class="text-xl text-slate-700">Company website</h3>
-          <p class="text-sm  text-slate-500">Vuejs Laravel</p>
-        </div>
-      </div>
+      -->
     </div>
   </div>
 </template>
